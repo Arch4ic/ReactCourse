6a7c65fb8a54e0d2a6ff67f511f530ca95d209be
emil mustonen <emppu.mustonen1@gmail.com>
2022-12-08 09:49:55 +0200

    Task 6 done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const urll ='https://api.github.com/search/repositories?q=react'
 function App() {
+  const [repos, setRepos] = useState([])
+
+  useEffect(() =>{
+    fetch(urll,{
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then((data) => setRepos(data.items))
+  }, []);
   return (
     <div className="App">
-      <h1>Welcome to my site</h1>
-      <p>This is my first React app</p>
+      <table>
+        <h1>Repositories</h1>
+        <tbody>
+          <tr>
+            <td style={{fontWeight: 'bold'}}>Name</td>
+            <td style={{fontWeight: 'bold'}}>Url</td>
+          </tr>
+        {
+          repos.map((item) =>
+           <tr>
+             <td>{item.full_name}</td>
+             <td>{item.url}</td>
+           </tr>
+          )
+        }
+        </tbody>
+      </table>
     </div>
   );
 }
